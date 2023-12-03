74519db6816ddce199aca5d51625e489bd5e79f1
daum88 <68275432+daum88@users.noreply.github.com>
2023-12-03 20:36:16 +0200

    home pages fixes, comments on java

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Example: Smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {  //tagab sujuva kerimise käitumise veebilehe ankrulinkidele. Kui kasutaja klikib lingil, mis viitab lehekülje sees olevale elemendi ID-le, kerib leht sujuvalt soovitud asukohta, mitte ei hüppa sinna kohe.
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
@@ -9,7 +8,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
+    const rect = element.getBoundingClientRect(); // Funktsioon kontrollib, kas antud element on kasutaja ekraanil nähtav (viewport'is). element.getBoundingClientRect() annab elemendi asukoha ja suuruse teavet, mille top, left, bottom, ja right väärtusi võrreldakse akna mõõtmetega.
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
@@ -18,7 +17,7 @@ function isInViewport(element) {
     );
 }
 
-function runOnScroll() {
+function runOnScroll() { //See funktsioon lisatakse akna scroll sündmusele ja käivitub, kui kasutaja lehekülge kerib. Funktsioon kontrollib iga .culture-image klassiga pildi puhul, kas see on viewport'is. Kui on, siis lisatakse pildile fade-in klass, mis tavaliselt käivitab visuaalse efekti, nagu näiteks aeglane ilmumine (fade in).
     document.querySelectorAll('.culture-image').forEach(img => {
         if (isInViewport(img)) {
             img.classList.add('fade-in');
@@ -26,4 +25,4 @@ function runOnScroll() {
     });
 }
 
-window.addEventListener('scroll', runOnScroll);
+window.addEventListener('scroll', runOnScroll); //See rida registreerib runOnScroll funktsiooni käivituma iga kord, kui kasutaja kerib lehekülge.
