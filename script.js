bde248e7dcd6912a66d1aa834107d9608d18ef9a
daum88 <68275432+daum88@users.noreply.github.com>
2023-12-04 18:31:01 +0200

    liigutasin js funktsiooni script.js faili

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {  //tagab sujuva kerimise käitumise veebilehe ankrulinkidele. Kui kasutaja klikib lingil, mis viitab lehekülje sees olevale elemendi ID-le, kerib leht sujuvalt soovitud asukohta, mitte ei hüppa sinna kohe.
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    /*
+    * tagab sujuva kerimise käitumise veebilehe ankrulinkidele. Kui kasutaja klikib lingil, mis viitab lehekülje sees olevale elemendi ID-le, kerib leht sujuvalt soovitud asukohta, mitte ei hüppa sinna kohe.
+    */
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
@@ -8,7 +11,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {  //tagab sujuva ke
 });
 
 function isInViewport(element) {
-    const rect = element.getBoundingClientRect(); // Funktsioon kontrollib, kas antud element on kasutaja ekraanil nähtav (viewport'is). element.getBoundingClientRect() annab elemendi asukoha ja suuruse teavet, mille top, left, bottom, ja right väärtusi võrreldakse akna mõõtmetega.
+    const rect = element.getBoundingClientRect(); 
+    /*
+    *  Funktsioon kontrollib, kas antud element on kasutaja ekraanil nähtav (viewport'is). element.getBoundingClientRect() annab elemendi asukoha ja suuruse teavet, mille top, left, bottom, ja right väärtusi võrreldakse akna mõõtmetega.
+    */
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
@@ -17,12 +23,19 @@ function isInViewport(element) {
     );
 }
 
-function runOnScroll() { //See funktsioon lisatakse akna scroll sündmusele ja käivitub, kui kasutaja lehekülge kerib. Funktsioon kontrollib iga .culture-image klassiga pildi puhul, kas see on viewport'is. Kui on, siis lisatakse pildile fade-in klass, mis tavaliselt käivitab visuaalse efekti, nagu näiteks aeglane ilmumine (fade in).
+function runOnScroll() { 
+    /* 
+    * See funktsioon lisatakse akna scroll sündmusele ja käivitub, kui kasutaja lehekülge kerib. Funktsioon kontrollib iga .culture-image klassiga pildi puhul, kas see on viewport'is. Kui on, siis lisatakse pildile fade-in klass, mis tavaliselt käivitab visuaalse efekti, nagu näiteks aeglane ilmumine (fade in).
+    */
     document.querySelectorAll('.culture-image').forEach(img => {
         if (isInViewport(img)) {
             img.classList.add('fade-in');
         }
     });
+}
+
+function redirectTo(page) {
+    window.location.href = '?page=' + page;
 }
 
 window.addEventListener('scroll', runOnScroll); //See rida registreerib runOnScroll funktsiooni käivituma iga kord, kui kasutaja kerib lehekülge.
